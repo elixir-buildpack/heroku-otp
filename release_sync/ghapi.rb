62ef166152802bf3405408bed79b8777a2458fd1
Kaz Walker <me@kaz.codes>
2021-01-18 18:52:55 -0800

    Fix release fetching by using GraphQL.

---
+++ release_sync/ghapi.rb
@@ -0,0 +1,32 @@
+require 'graphql/client'
+require 'graphql/client/http'
+
+module GHAPI
+  HTTP = GraphQL::Client::HTTP.new('https://api.github.com/graphql') do
+    def headers(context)
+      if !ENV['GITHUB_TOKEN'].nil?
+        { 'Authorization': "Bearer #{ENV['GITHUB_TOKEN']}" }
+      else
+        {}
+      end
+    end
+  end
+  Schema = GraphQL::Client.load_schema(HTTP)
+  Client = GraphQL::Client.new(schema: Schema, execute: HTTP)
+
+  GetReleases = Client.parse <<-GRAPHQL
+    query($repo_owner: String!, $repo_name: String!, $after: String) {
+      repository(name: $repo_name, owner: $repo_owner) {
+        releases(orderBy: {field: NAME, direction: DESC}, first: 100, after: $after) {
+          totalCount
+          nodes {
+            name
+          }
+          edges {
+            cursor
+          }
+        }
+      }
+    }
+  GRAPHQL
+end
